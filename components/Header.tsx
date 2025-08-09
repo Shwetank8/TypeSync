fa3bd317a95e5315a2a1447f0d9097c8434e49dc
Shwetank8 <sinhashwetank74@gmail.com>
2025-08-10 03:16:38 +0530

    ui changes to header and layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,22 @@ import Link from 'next/link'
 const Header = ({ children, className }: HeaderProps) => {
   return (
     <div className={cn("header", className)}>
-      <Link href='/' className="md:flex-1">
+      <Link href='/' className="md:flex-1 flex flex-row items-center justify-start ">
+      <Image 
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={40}
+          height={40}
+          className=" md:block"
+        />
         <Image 
-          src="/assets/icons/logo.svg"
+          src="/assets/icons/logo-type.png"
           alt="Logo with name"
           width={120}
           height={32}
           className="hidden md:block"
         />
-        <Image 
-          src="/assets/icons/logo-icon.svg"
-          alt="Logo"
-          width={32}
-          height={32}
-          className="mr-2 md:hidden"
-        />
+        
       </Link>
       {children}
     </div>
